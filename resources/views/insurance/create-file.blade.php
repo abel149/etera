cb7915956b004823193ba81f9e6f094e42e18776
Abel Ashenafi <ashenafiabel66@gmail.com>
2026-05-19 11:29:04 +0300

    Add input mode numeric to the chansis number

--- resources/views/insurance/create-file.blade.php
+++ resources/views/insurance/create-file.blade.php
@@ -231,6 +231,7 @@
        pattern="[A-Za-z0-9]{17}"
        placeholder="Enter Chassis Number"
        value="{{ old('chassis_number') }}"
+       inputmode="numeric"
        required
        oninvalid="this.setCustomValidity('Please enter a valid 17-character chassis number')"
        oninput="this.setCustomValidity('')">
